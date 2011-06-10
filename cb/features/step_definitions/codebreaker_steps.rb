8f9c8b3b24fa5197ef19cbaf3ea1f128cdc791f9
Ayumu AIZAWA <ayumu.aizawa@accenture.com>
2011-06-11 03:05:20 +0900

    Add step definition for Codebreaker

---
+++ cb/features/step_definitions/codebreaker_steps.rb
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+Given /^I am not yet playing$/ do
+end
+
+When /^I start a game$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^the secret code is "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I guess "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the mark should be "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+
