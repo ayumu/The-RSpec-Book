d37dcafccdf064b4e2b5c0a52ce0417eb5b28789
Ayumu AIZAWA <ayumu.aizawa@accenture.com>
2011-06-11 02:19:21 +0900

    Add step definition

---
+++ hello/features/step_definitions/greeter_steps.rb
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+Given /^a greeter$/ do
+  @greeter = CucumberGreeter.new
+end
+
+When /^I send it the greet message$/ do
+  @message = @greeter.greet
+end
+
+Then /^I should see "([^"]*)"$/ do |greeting|
+  @message.should == greeting
+end
