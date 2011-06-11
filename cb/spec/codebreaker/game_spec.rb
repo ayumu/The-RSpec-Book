962dd33c4d0cde5847a3e5919df92847ef73f0b8
Ayumu AIZAWA <ayumu.aizawa@accenture.com>
2011-06-11 22:40:44 +0900

    create double as null object

--- cb/spec/codebreaker/game_spec.rb
+++ cb/spec/codebreaker/game_spec.rb
@@ -6,7 +6,7 @@ module Codebreaker
   describe Game do
     describe "#start" do
       it "sends a welcome message" do
-        output = double('output')
+        output = double('output').as_null_object
         game = Game.new(output)
 
         output.should_receive(:puts).with('Welcome to Codebreaker!')
@@ -14,7 +14,7 @@ module Codebreaker
         game.start
       end
       it "prompts for the first guess" do
-        output = double('output')
+        output = double('output').as_null_object
         game = Game.new(output)
 
         output.should_receive(:puts).with('Enter guess:')
